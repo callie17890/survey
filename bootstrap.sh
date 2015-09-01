ae0d9fb37167162a4e6a89faf1f3ca83e3062b0d
evan <evan@sdicg.com>
2015-09-01 17:53:36 -0400

    updating bootstrap for error log reading

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,10 @@
   sudo add-apt-repository ppa:ondrej/php5-5.6
   sudo apt-get install -y apache2 apache2-utils apache2-mpm-prefork php5 php5-cli 
 
-  chown -R vagrant /var/log/apache2/*
+  chgrp -R vagrant /var/log/apache2
 
   rm -rf /var/www/html
   ln -s /vagrant/www /var/www/html
-  ln -s /var/log/apache2/error.log /vagrant
 
   sudo apache2ctl restart
+
